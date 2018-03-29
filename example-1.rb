4098c49cf44b6ea4b10491b56b3b5e0db567f85a
ayadij <ayadijkwel@yahoo.com>
2018-03-29 10:32:51 -0600

    ex1 added

---
+++ example-1.rb
@@ -0,0 +1,18 @@
+class Ducati
+end
+
+class Ducati
+  def rev
+    "vroom"
+  end
+end
+
+
+class Ducati 
+  def rev
+    "vroom vroom"
+  end
+end
+
+# monkey patching is like a jr version of metaprogramming
+# ruby you can modify as far up as you want, even as far up as the base class
